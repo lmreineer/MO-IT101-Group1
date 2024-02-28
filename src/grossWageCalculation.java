49e1efe74134993d7103a2f860dda3277c530c55
lance <lmreineer@gmail.com>
2024-02-28 10:27:47 +0800

    Add SSS and PhilHealth deductions

--- src/grossWageCalculation.java
+++ src/grossWageCalculation.java
@@ -1,12 +1,13 @@
 import java.text.DecimalFormat;
 
 public class grossWageCalculation {
-  EmployeeInfo info = new EmployeeInfo();
-
-  DecimalFormat decimal = new DecimalFormat("0.00");
 
   public String calculateGrossWage(int employeeNumInput) {
-    // 160 because 8 x 5 is 40 and 40 x 4 = 160
+    EmployeeInfo info = new EmployeeInfo();
+
+    DecimalFormat decimal = new DecimalFormat("0.00");
+
+    // 160 because: 8 x 5 is 40 and 40 x 4 = 160
     double grossWage = info.hourlyRate[employeeNumInput] * 160;
     String formattedGrossWage = decimal.format(grossWage);
 
--- src/netWageCalculation.java
+++ src/netWageCalculation.java
@@ -3,21 +3,73 @@ import java.text.DecimalFormat;
 public class netWageCalculation {
   grossWageCalculation grossWage = new grossWageCalculation();
 
-  EmployeeInfo info = new EmployeeInfo();
-
   DecimalFormat decimal = new DecimalFormat("0.00");
 
   public String calculateSssContribution(int employeeNumInput) {
-    double netWage = 0;
+    double sssDeduction = 0;
+
+    SssContributionList sss = new SssContributionList();
 
     double doubledGrossWage = Double.parseDouble(grossWage.calculateGrossWage(employeeNumInput));
-    if (doubledGrossWage > 24750) {
-      netWage = doubledGrossWage - 1125;
+
+    // Loop through the compensation ranges and apply deduction based on contribution
+    for (int i = 0; i < sss.compensationRanges.length; i++) {
+      // Separate the values
+      String[] asd = sss.compensationRanges[i].split("-");
+      // Convert to double and assign both values to their sides based on the SSS Contribution sheet
+      double leftValue = Double.parseDouble(asd[0]);
+      double rightValue = Double.parseDouble(asd[1]);
+
+      // Check the range of which the employee's grossWage falls between
+      if (doubledGrossWage > leftValue && doubledGrossWage <= rightValue) {
+        // Apply deduction based on contribution
+        // "contribution[i]"'s logic: ONLY return the value if the condition is true
+        sssDeduction = doubledGrossWage - sss.contributions[i];
+      } else if (doubledGrossWage <= leftValue) {
+        // Subtract grossWage with the first element of the contributions array
+        sssDeduction = doubledGrossWage - sss.contributions[0];
+      } else if (doubledGrossWage > rightValue) {
+        // Subtract grossWage with the last element of the contributions array
+        sssDeduction = doubledGrossWage - sss.contributions[sss.contributions.length - 1];
+      }
     }
 
-    String formattedNetWage = decimal.format(netWage);
+    String formattedSssDeduction = decimal.format(sssDeduction);
 
-    return formattedNetWage;
+    return formattedSssDeduction;
+  }
+
+  public String calculatePhilHealthContribution(int employeeNumInput) {
+    double monthlyPremium = 0;
+    double employeeShare = 0;
+
+    double doubledGrossWage = Double.parseDouble(grossWage.calculateGrossWage(employeeNumInput));
+
+    // If grossWage is less than the minimum basic salary
+    if (doubledGrossWage < 10000) {
+      // Deduct a minimum contribution of 300
+      monthlyPremium = 300;
+      // Deduct 50% from the monthlyPremium
+      employeeShare = (50 * monthlyPremium) / 100;
+
+      // Else if grossWage is more than the maximum basic salary
+    } else if (doubledGrossWage > 60000) {
+      // Deduct a maximum contribution of 1800
+      monthlyPremium = 1800;
+      // Deduct 50% from the monthlyPremium
+      employeeShare = (50 * monthlyPremium) / 100;
+
+      // Else if it falls between the range of 10000.01 - 59999.99
+    } else {
+      // Deduct 3% from the total grossWage
+      monthlyPremium = (3 * doubledGrossWage) / 100;
+      // Deduct 50% from the monthlyPremium
+      employeeShare = (50 * monthlyPremium) / 100;
+    }
+
+    String formattedPhilHealthDeduction = decimal.format(employeeShare);
+
+    return formattedPhilHealthDeduction;
   }
 
   public String calculateNetWage(int employeeNumInput) {
