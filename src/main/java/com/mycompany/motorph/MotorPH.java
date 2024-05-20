ca3a05f962e69eb4b75b1636218b7dfa27e49bdd
lance <lmreineer@gmail.com>
2024-05-20 10:04:11 +1000

    Combine MotorPH and MotorPHApplication codes to lessen tight coupling code

--- src/main/java/com/mycompany/motorph/MotorPH.java
+++ src/main/java/com/mycompany/motorph/MotorPH.java
@@ -10,12 +10,24 @@ import java.util.Scanner;
 /**
  * Main class for the MotorPH payroll system.
  * <p>
- * This class runs the application in a loop for prompts and user input using
- * the console.
+ * This class interacts with the user, displays menus, takes user input, and
+ * performs corresponding actions based on the input using the console in a
+ * loop.
  *
  * @author Lance1
  */
 public class MotorPH {
+
+    private final MotorPHManager motorPhManager;
+
+    /**
+     * Constructor for MotorPH.
+     * <p>
+     * Initializes an instance of MotorPHManager.
+     */
+    public MotorPH() {
+        this.motorPhManager = new MotorPHManager();
+    }
 
     /**
      * Entry point of the application.
@@ -28,12 +40,49 @@ public class MotorPH {
         // Use Scanner for user input
         Scanner scanner = new Scanner(System.in);
 
-        // Create an instance of MotorPHApplication
-        MotorPHApplication motorPhApplication = new MotorPHApplication();
-
         // Run the application in a loop
         while (true) {
-            motorPhApplication.run(scanner);
+            new MotorPH().run(scanner);
+        }
+    }
+
+    /**
+     * Executes the main function of the MotorPH payroll system.
+     * <p>
+     * Displays menus, takes user input, and performs corresponding actions.
+     *
+     * @param scanner The scanner object for user input
+     * @throws ParseException If a parsing error occurs
+     * @throws IOException If an I/O error occurs
+     */
+    public void run(Scanner scanner) throws ParseException, IOException {
+        // Display the menu
+        motorPhManager.printMenu();
+
+        // Get user input
+        int menuChoice = motorPhManager.getMenuChoice(scanner);
+
+        // Perform action based on user input
+        switch (menuChoice) {
+            case 1:
+                // Show employee information
+                motorPhManager.showEmployeeInformation(scanner);
+                break;
+            case 2:
+                // Show gross wage
+                motorPhManager.showWage(scanner, true);
+                break;
+            case 3:
+                // Show net wage 
+                motorPhManager.showWage(scanner, false);
+                break;
+            case 0:
+                // Exit the program
+                System.out.println("Logged out.");
+                System.exit(0);
+            default:
+                System.out.println("\nInvalid option. Please try again.");
+                break;
         }
     }
 }
